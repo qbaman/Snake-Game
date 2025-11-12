59bc5d557b89b7728c3e9ab82983f706d944d1b1
qbaman <78668489+qbaman@users.noreply.github.com>
2025-11-12 13:42:27 +0000

    feat: deque-based snake movement and collision

---
+++ src/game/snake.py
@@ -0,0 +1,27 @@
+from collections import deque
+
+class Snake:
+    def __init__(self, start):
+        self.body = deque([start, (start[0]-1, start[1]), (start[0]-2, start[1])])
+        self.dir = (1, 0)
+        self.grow_pending = 0
+
+    def set_dir(self, dx, dy):
+        if (dx, dy) == (-self.dir[0], -self.dir[1]):  # block 180° turns
+            return
+        self.dir = (dx, dy)
+
+    def head(self):
+        return self.body[0]
+
+    def move(self):
+        hx, hy = self.head()
+        nx, ny = hx + self.dir[0], hy + self.dir[1]
+        self.body.appendleft((nx, ny))
+        if self.grow_pending:
+            self.grow_pending -= 1
+        else:
+            self.body.pop()
+
+    def grow(self, n=1):
+        self.grow_pending += n
--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import pygame
 from .game.constants import *
+from .game.snake import Snake
 
 def draw_grid(surf):
     for x in range(0, WINDOW_W, CELL):
@@ -7,20 +8,42 @@ def draw_grid(surf):
     for y in range(0, WINDOW_H, CELL):
         pygame.draw.line(surf, GREY, (0, y), (WINDOW_W, y))
 
+def draw_cell(surf, pos, color):
+    x, y = pos
+    pygame.draw.rect(surf, color, (x*CELL, y*CELL, CELL, CELL))
+
+def in_bounds(x, y):
+    return 0 <= x < GRID_W and 0 <= y < GRID_H
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((WINDOW_W, WINDOW_H))
     pygame.display.set_caption("Unit 24 Snake")
     clock = pygame.time.Clock()
+
+    snake = Snake((GRID_W//2, GRID_H//2))
     running = True
     while running:
         for e in pygame.event.get():
-            if e.type == pygame.QUIT:
-                running = False
+            if e.type == pygame.QUIT: running = False
+            elif e.type == pygame.KEYDOWN:
+                if e.key == pygame.K_UP: snake.set_dir(0, -1)
+                elif e.key == pygame.K_DOWN: snake.set_dir(0, 1)
+                elif e.key == pygame.K_LEFT: snake.set_dir(-1, 0)
+                elif e.key == pygame.K_RIGHT: snake.set_dir(1, 0)
+
+        snake.move()
+        hx, hy = snake.head()
+        if not in_bounds(hx, hy) or (hx, hy) in list(snake.body)[1:]:
+            running = False
+
         screen.fill(BLACK)
         draw_grid(screen)
+        for seg in snake.body:
+            draw_cell(screen, seg, (0, 200, 0))
         pygame.display.flip()
         clock.tick(FPS)
+
     pygame.quit()
 
 if __name__ == "__main__":
